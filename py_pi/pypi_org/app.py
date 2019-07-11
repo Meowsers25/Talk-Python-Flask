c65d4b3b2a1b4c28020e22cc692d911065627ad1
Meowsers25 <c.lamothe25@gmail.com>
2019-07-10 22:04:56 -0400

    First Jinja template

--- py_pi/pypi_org/app.py
+++ py_pi/pypi_org/app.py
@@ -2,9 +2,17 @@ import flask
 
 app = flask.Flask(__name__)
 
+def get_latest_packages():
+    return [
+        {'name': 'Flask', 'version': '1.2.3'},
+        {'name': 'SQL', 'version': '2.2.0'},
+        {'name': 'Passlib', 'version': '1.0.0'},
+    ]
+
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    test_packages = get_latest_packages()
+    return flask.render_template('index.html', packages=test_packages)
 
 if __name__ == "__main__":
     app.run(debug=True)
