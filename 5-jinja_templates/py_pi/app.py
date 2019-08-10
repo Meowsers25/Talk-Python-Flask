d2eeb53d1d5b3367b45bd3e933d8a11639927966
Christian Lamothe <c.lamothe25@gmail.com>
2019-08-10 10:34:33 -0400

    Working with jinja templates

---
+++ 5-jinja_templates/py_pi/app.py
@@ -0,0 +1,23 @@
+import flask
+
+app = flask.Flask(__name__)
+
+def get_latest_packages():
+    return [
+        {'name': 'flask', 'version': '1.2.3'},
+        {'name': 'sqlalchemy', 'version': '4.5.6'},
+        {'name': 'poptart', 'version': '7.8.9'},
+
+    ]
+
+@app.route('/')
+def index():
+    test_packages = get_latest_packages()
+    return flask.render_template('index.html', packages=test_packages)
+
+@app.route('/about')
+def about():
+    return flask.render_template('/about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)    
